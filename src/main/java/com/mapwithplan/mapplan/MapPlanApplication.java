1cb713c06e602b20f91ed15f59467187f01d3a0f
zoo-code <75787789+zooo-code@users.noreply.github.com>
2024-01-01 10:17:53 +0900

    Initial commit

---
+++ src/main/java/com/mapwithplan/mapplan/MapPlanApplication.java
@@ -0,0 +1,13 @@
+package com.mapwithplan.mapplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapPlanApplication.class, args);
+	}
+
+}
